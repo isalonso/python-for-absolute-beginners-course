1464d20a58436b9d557c6417dd07fff8ca5210b5
Isabel Alonso <isalonso@proton.me>
2023-01-15 17:38:12 -0500

    my first commit

--- code/05-interactive-code/m-n-m/guessinggame.py
+++ code/05-interactive-code/m-n-m/guessinggame.py
@@ -7,14 +7,14 @@ print("------------------------------")
 print("Guess the number of M&Ms and you get lunch on the house!")
 print()
 
-mm_count = random.randint(1, 100)
-attempt_limit = 5
+mm_count = random.randint(1, 120)
+attempt_limit = 1
 attempts = 0
 
 while attempts < attempt_limit:
     guess_text = input("How many M&Ms are in the jar? ")
     guess = int(guess_text)
-    attempts += 1
+    attempts += 3
 
     if mm_count == guess:
         print(f"You got a free lunch! It was {guess}.")
